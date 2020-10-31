54462df9875c8d806922583b6fdab9e4840b9eec
codydeatherage <cody@DESKTOP-DCKI10R.localdomain>
2020-10-30 22:36:24 -0700

    implemented eventListeners for arrow keys

--- src/index.js
+++ src/index.js
@@ -1,14 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import GameBoard from './components/GameBoard';
+import GameInfoPanel from './components/GameInfoPanel';
+import GameBoardItem from './components/GameBoardItem';
 import reportWebVitals from './reportWebVitals';
 
+let boardState = [
+  0, 1, 0, 0,
+  0, 0, 0, 0,
+  0, 0, 0, 0,
+  0, 0, 5, 0
+];
+document.onkeydown = checkKey;
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.key === 'ArrowUp') {
+      // up arrow
+  }
+  else if (e.key === 'ArrowDown') {
+      // down arrow
+  }
+  else if (e.key === 'ArrowLeft') {
+     // left arrow
+  }
+  else if (e.key === 'ArrowRight') {
+     // right arrow
+  }
+
+}
+
+document.addEventListener('keydown', function(e) {
+  switch (e.key) {
+      case 'ArrowLeft':
+          console.log('left');
+          break;
+      case 'ArrowUp':
+        console.log('up');
+          break;
+      case 'ArrowRight':
+        console.log('right');
+          break;
+      case 'ArrowDown':
+        console.log('down');
+          break;
+  }
+});
 ReactDOM.render(
   <React.StrictMode>
-    <div className="App">
-      <header className="App-header">
-        Hello, World
-      </header>
+    <div className = "App">
+      <GameInfoPanel></GameInfoPanel>
+      <br></br>
+      <GameBoard
+        boardState = {boardState}
+      ></GameBoard>
+      <div className="bot-pad"></div>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
