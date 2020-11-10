e45f6e85d2c1bc4708b907d16ff70066c235da65
codydeatherage <cdeatherage14@gmail.com>
2020-11-10 06:21:24 -0800

    added changing colors

--- src/components/GameBoardItem.js
+++ src/components/GameBoardItem.js
@@ -9,14 +9,23 @@ class GameBoardItem extends Component{
         console.log(e.keyCode); 
         console.log(e.code);
     }
-      
+    setBGColor = () =>{
+        let color = '';
+        switch(this.props.value){
+            case 2: color = 'rgb(246, 242, 222)';break;
+            case 4: color = '#ffeaa7';break;
+            case 8: color = '#fdcb6e';break;
+        }
+
+        return color;
+    }
     
     render(){
         let id = this.props.id;
         if(this.props.value > 0){
             return(
-                <div onKeyDown={this.keyDown} className="game-piece" id={id}>
-                    <h1>{this.props.value}</h1>
+                <div onKeyDown={this.keyDown} className={`game-piece tile-${this.props.value}`} id={id}>
+                   {this.props.value}
                 </div>
             )
         }else{
